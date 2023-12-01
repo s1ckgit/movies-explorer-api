5fd812eb1915051935c96522502ffbd1dfacae9a
Kazzzemir <s1ckwell@mail.ru>
2023-12-01 16:47:40 +0300

    change port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routers/index');
 const errorHandler = require('./middlewares/errorHandler');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
